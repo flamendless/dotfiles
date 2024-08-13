532052e653788cb4a262932b22c2d569b5ea0296
flamendless <flamendless8@gmail.com>
2024-08-13 11:09:11 +0800

    Update wezterm config

--- .wezterm.lua
+++ .wezterm.lua
@@ -1,13 +1,15 @@
-local wezterm = require 'wezterm'
+local wezterm = require("wezterm")
 
 local config = wezterm.config_builder()
 
-config.color_scheme = 'GruvboxDark'
-config.default_domain = 'WSL:Arch'
+config.color_scheme = "GruvboxDark"
+config.default_domain = "WSL:Arch"
+
+config.font = wezterm.font("JetBrains Mono")
+config.font_size = 10.0
+
 config.window_background_opacity = 1.0
 config.hide_tab_bar_if_only_one_tab = true
-
-config.font = wezterm.font('JetBrains Mono')
-config.font_size = 10.0
+config.audible_bell = "Disabled"
 
 return config
